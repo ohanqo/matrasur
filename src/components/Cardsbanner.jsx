d6fb776ee7e9d9fd95e658feba13e3f36e7aefd5
ohanqo <gm.anqo@gmail.com>
2018-04-19 17:38:05 +0200

    Ajout du cardsbanner

---
+++ src/components/Cardsbanner.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+
+export default class Cardsbanner extends Component {
+    render() {
+        return(
+            <div className="o-cardsbanner">
+                <div className="m-card">
+                    <div className="m-card__title">Omnem defensio omnem existimatis.</div>
+                    <div className="m-card__content">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
+                    <div className="m-card__button"></div>
+                </div>
+                <div className="m-card">
+                    <div className="m-card__title">Omnem defensio omnem existimatis.</div>
+                    <div className="m-card__content">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
+                    <div className="m-card__button"></div>
+                </div>
+                <div className="m-card">
+                    <div className="m-card__title">Omnem defensio omnem existimatis.</div>
+                    <div className="m-card__content">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
+                    <div className="m-card__button"></div>
+                </div>
+                <div className="m-card">
+                    <div className="m-card__title">Omnem defensio omnem existimatis.</div>
+                    <div className="m-card__content">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
+                    <div className="m-card__button"></div>
+                </div>
+            </div>
+        );
+    }
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 
 import Navbar from '../components/Navbar';
+import Cardsbanner from '../components/Cardsbanner';
 
 export default class Home extends Component {
     render() {
@@ -17,6 +18,7 @@ export default class Home extends Component {
                     <div><h2 className="a-title -big">Lorem ipsum <br/><span className="a-sep"></span></h2></div>
                     <p className="a-subtitle">Ponderum nec nullas arta lapsantibus ponderum et nullas ad nostri plantis consternuntur eos dumos vertices.</p>
                 </div>
+                <Cardsbanner/>
             </div>
         );
     }
