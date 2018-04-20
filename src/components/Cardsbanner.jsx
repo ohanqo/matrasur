f0cc1c32e4458e648d4f0e849bf89872b7a51394
ohanqo <gm.anqo@gmail.com>
2018-04-20 11:19:32 +0200

    Home > cardsbanner

--- src/components/Cardsbanner.jsx
+++ src/components/Cardsbanner.jsx
@@ -5,25 +5,27 @@ export default class Cardsbanner extends Component {
     render() {
         return(
             <div className="o-cardsbanner">
-                <div className="m-card">
-                    <div className="m-card__title">Omnem defensio omnem existimatis.</div>
-                    <div className="m-card__content">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
-                    <div className="m-card__button"></div>
-                </div>
-                <div className="m-card">
-                    <div className="m-card__title">Omnem defensio omnem existimatis.</div>
-                    <div className="m-card__content">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
-                    <div className="m-card__button"></div>
-                </div>
-                <div className="m-card">
-                    <div className="m-card__title">Omnem defensio omnem existimatis.</div>
-                    <div className="m-card__content">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
-                    <div className="m-card__button"></div>
-                </div>
-                <div className="m-card">
-                    <div className="m-card__title">Omnem defensio omnem existimatis.</div>
-                    <div className="m-card__content">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
-                    <div className="m-card__button"></div>
+                <div className="o-banner">
+                    <div className="m-card card-body">
+                        <div className="m-card__title card-title">Omnem defensio omnem existimatis.</div>
+                        <div className="m-card__content card-text">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
+                        <button type="button" className="m-card__button btn">Lorem ipsum</button>
+                    </div>
+                    <div className="m-card card-body">
+                        <div className="m-card__title card-title">Omnem defensio omnem existimatis.</div>
+                        <div className="m-card__content card-text">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
+                        <button type="button" className="m-card__button btn">Lorem ipsum</button>
+                    </div>
+                    <div className="m-card card-body">
+                        <div className="m-card__title card-title">Omnem defensio omnem existimatis.</div>
+                        <div className="m-card__content card-text">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
+                        <button type="button" className="m-card__button btn">Lorem ipsum</button>
+                    </div>
+                    <div className="m-card card-body">
+                        <div className="m-card__title card-title">Omnem defensio omnem existimatis.</div>
+                        <div className="m-card__content card-text">Intactam nostris fore milite parte documentis latrones intactam inpares deviis Lycaoniam cum magna deviis Pamphyliam diu magna et praesidiis igitur.</div>
+                        <button type="button" className="m-card__button btn">Lorem ipsum</button>
+                    </div>
                 </div>
             </div>
         );
